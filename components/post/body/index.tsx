857968428f903a9aae4872f23882873f679f6057
Chin <greeting.to.chin@gmail.com>
2023-01-11 23:52:00 +0800

    feat(post/body): support markdown to react content

--- components/post/body/index.tsx
+++ components/post/body/index.tsx
@@ -1,37 +1,13 @@
-import type { Document } from '@contentful/rich-text-types'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import { BLOCKS } from '@contentful/rich-text-types'
-import markdownStyles from './markdown.module.css'
-import RichTextAsset from './rich-text-asset'
+import { useRemark } from 'react-remark';
 
-export type Content = {
-  json: Document;
-  links: {
-    assets: {
-      block: any
-    }
-  }
-}
-
-const customMarkdownOptions = (content: Content) => ({
-  renderNode: {
-    [BLOCKS.EMBEDDED_ASSET]: (node: any) => (
-      <RichTextAsset
-        id={node.data.target.sys.id}
-        assets={content.links.assets.block}
-      />
-    ),
-  },
-})
-
-export default function PostBody({ content }: { content: Content }) {
+export default function PostBody({ content }: { content: string }) { 
+  const [reactContent, setMarkdownSource] = useRemark();
+  setMarkdownSource(content)
+ 
   return (
     <div max-w="2xl" mx="auto">
-      <div className={markdownStyles['markdown']}>
-        {documentToReactComponents(
-          content.json,
-          customMarkdownOptions(content)
-        )}
+      <div>
+        {reactContent}
       </div>
     </div>
   )
