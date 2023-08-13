d430196afae4ecc58a621ab3816f5e82141b9ce9
Chin <greeting.to.chin@gmail.com>
2023-08-13 19:41:22 +0800

    feat: update about me

--- components/me/index.tsx
+++ components/me/index.tsx
@@ -2,9 +2,16 @@ export default function Me() {
   return (
     <section>
       <h2 text="3xl" mb="3">這是 Chin ，</h2>
-      <p m="0">她喜歡整齊一致的東西。</p>
+      <p m="0">
+        她喜歡整齊一致的東西。
+      </p>
       <h2 text="3xl" mt="30" mb="3">Chin 是誰 ？</h2>
-      <p m="0" mb="30">擅長從問題抽絲剝繭找到需求的 Problem Solver，<br />透過程式碼或是其他方式。</p>
+      <p m="0" mb="30" whitespace="nowrap md:normal">
+        擅長從問題抽絲剝繭找到需求的 <strong>Problem Solver</strong>，<br />
+        透過程式碼或是其他方式。<br />
+        <span display="inline-block" mt="4">這些是常見的稱謂：</span><br />
+        資深軟體工程師 / 奈米創業家 / 生活思考家 / 意義探索者
+      </p>
     </section>
   )
 }
